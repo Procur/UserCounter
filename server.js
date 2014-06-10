f21834df4f748507f488ec160c026f8070a2e13b
Will Ricketts <wricketts@procur.com>
2014-06-10 17:46:39 -0400

    lol

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var mongo = require('mongodb'),
-  http = require('http'),
-  monk = require('monk'),
-  db = monk('procur-dev:Pr0cur1!@oceanic.mongohq.com:10073/app25459603'),
-  io = require('socket.io').listen(1337),
-  fs = require('fs');
-
 function loop(){
+  var mongo = require('mongodb'),
+    http = require('http');
+    monk = require('monk'),
+    db = monk('procur-dev:Pr0cur1!@oceanic.mongohq.com:10073/app25459603'),
+    collection = db.get('user'),
+    io = require('socket.io').listen(1337);
+
+
   io.sockets.on('connection', function(socket){
-    var collection = db.get('user');
     collection.count({}, function(err, count){
       socket.emit('count', count);
       console.log('counted');
@@ -15,9 +15,5 @@ function loop(){
     });
   });
 }
-var index = fs.readFileSync('index.html');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(index);
-}).listen(1338);
+loop();
